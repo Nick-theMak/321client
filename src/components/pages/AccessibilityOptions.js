42aa692fc62137f51f88963bada5945fd672ac0c
Jared Juangco <jjuangco01@gmail.com>
2024-07-28 13:02:08 +1000

    Implemented accessibility options

---
+++ src/components/pages/AccessibilityOptions.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DrawerAppBar from '../elements/DrawerAppBar';
+import ContainerCard from '../elements/ContainerCard';
+
+const AccessibilityOptions = () => {
+  return (
+    <div>
+      <DrawerAppBar></DrawerAppBar>
+      <ContainerCard/>
+    </div>
+  );
+};
+
+export default AccessibilityOptions;
