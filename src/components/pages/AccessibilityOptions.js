3fb97cb73cd6ca6669aae1c47b731285c53e0cdf
Jared Juangco <jjuangco01@gmail.com>
2024-07-28 16:21:19 +1000

    Removed drawer bar in AccessibilityOptions.js

--- src/components/pages/AccessibilityOptions.js
+++ src/components/pages/AccessibilityOptions.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import DrawerAppBar from '../elements/DrawerAppBar';
 import ContainerCard from '../elements/ContainerCard';
 
 const AccessibilityOptions = () => {
   return (
     <div>
-      <DrawerAppBar></DrawerAppBar>
       <ContainerCard/>
     </div>
   );
